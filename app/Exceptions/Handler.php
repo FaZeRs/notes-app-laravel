504609181161d10dd3707c7a2275b56c5ca44559
Nauris Linde <naurislinde@gmail.com>
2018-10-25 18:19:49 +0300

    Return json response when ModelNotFoundException is thrown

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -40,12 +40,18 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Exception               $exception
      * @return \Illuminate\Http\Response
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof \Illuminate\Database\Eloquent\ModelNotFoundException) {
+            return response()->json([
+                'message' => 'Resource not found',
+            ], 404);
+        }
+
         return parent::render($request, $exception);
     }
 }
