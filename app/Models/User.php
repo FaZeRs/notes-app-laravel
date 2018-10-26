8e4ffeaf2132eed3d88ba85e827b0e99f3da038d
Nauris Linde <naurislinde@gmail.com>
2018-10-26 16:09:54 +0300

    Formatting

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,10 @@
 namespace App\Models;
 
 use App\Traits\UuidScopeTrait;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
