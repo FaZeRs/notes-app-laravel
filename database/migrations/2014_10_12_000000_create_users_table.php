0caa56340ae53c253b938f70a6de11c8faf38e14
Nauris Linde <naurislinde@gmail.com>
2018-10-26 15:46:31 +0300

    Added remember token & email_verified_at field to users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,9 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->uuid('uuid')->index()->unique();
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
         });
